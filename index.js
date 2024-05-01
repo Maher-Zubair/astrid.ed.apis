1290a7d21e03d95a5ee7e1372595bbf9c6195e18
D3centX <109889758+Diegoson@users.noreply.github.com>
2024-05-01 07:28:41 +0200

    vorterx

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const ringtone = require('./function'); 
 const youtube = require('youtube-sr');
+const ChatWithGpt = require('./function');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 
+app.post('/gpt-api', async (req, res) => {
+  const textMessage = req.body.textMessage;
+  try {
+    const response = await ChatWithGpt(textMessage);
+    res.json({
+      status: 200,
+      owner: "Diegoson",
+      result: response,
+      example: `Hello: ${response}`
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 500,
+      owner: "Diegoson",
+      error: error,
+      example: `Hello: ${error}`
+    });
+  }
+});
+                            
 app.get('/search', async (req, res) => {
     const query = req.query.q;
     if (!query) {
