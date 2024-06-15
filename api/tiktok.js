e85d1aa23d547b7e85bf24dc809804bd2ef74e2a
D3centX <109889758+Diegoson@users.noreply.github.com>
2024-06-15 17:55:14 +0200

    🐊

--- api/tiktok.js
+++ api/tiktok.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { scrapeTikTok } = require('./scraper');
+const { scrapeTikTok } = require('../scraper/scrapper');
 
 const app = express();
 const port = 3000;
