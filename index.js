b9a3cb5219cb049db0a1794a8facf71563d4b9d6
ɪɴɴᴏxᴇɴᴛ ᴢᴜʙɪ <HELP_SIGMA-MD@outlook.com>
2024-06-15 23:19:33 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const ringtone = require('./scraper/function'); 
 const app = express();
+const gay = console.log
+const {searchYouTube} = require('./scraper/ytSearch')
 
                             
 app.get('/search', async (req, res) => {
@@ -16,6 +18,21 @@ app.get('/search', async (req, res) => {
     }
 });
 
-app.listen(300, () => {
+app.get('/search/yt', async(req, res) =>{
+    const q = req.query.q
+    if (!q) {
+        return res.status(400).json({ error: 'Query parameter "q" is required' });
+    }
+    await searchYouTube(q)
+    .then(data =>{
+        return res.json({result:data})
+    })
+    .catch(e =>{
+        return res.json({gay_error:e})
+        gay(e)
+    })
+})
+
+app.listen(8000, () => {
    console.log('Server listening on port 3000');
  });
